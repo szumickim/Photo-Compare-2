b162e0e25e23abd970ac517d191e9dd35c2c9d83
szumickim <48274414+szumickim@users.noreply.github.com>
2023-09-07 07:32:59 +0200

    Split ShowAllSummary and STEPShowAllSummary to 2 different excels

--- data/excelWorkspace.py
+++ data/excelWorkspace.py
@@ -25,11 +25,12 @@ PRODUCTS_ON_SCREEN: int = 3
 SHOW_ALL_SUMMARY_FORMAT: int = 1
 SUMMARY_FORMAT: int = 2
 
+
 def format_excel(final_df, file_name, format_type):
     # Stworzenie pustego obiektu xlsx
     writer = pd.ExcelWriter(file_name)
 
-    #Formatowanie (tutaj dzieje się magia)
+    # Formatowanie (tutaj dzieje się magia)
     format_sheet(writer, "sheet1", final_df, format_type)
 
     # Zamknięcie obiektu ze sformatowanym raportem
@@ -96,7 +97,8 @@ def format_sheet(writer, sheet_name, df, format_type):
 
 
 def create_summary_excel(products_collection, folder_to_save):
-    summary_columns = [SummaryConst.PRODUCT_ID, SummaryConst.FIRST_PHOTO_ID, SummaryConst.FIRST_REFERENCE, SummaryConst.HEIGHT,
+    summary_columns = [SummaryConst.PRODUCT_ID, SummaryConst.FIRST_PHOTO_ID, SummaryConst.FIRST_REFERENCE,
+                       SummaryConst.HEIGHT,
                        SummaryConst.WIDTH, SummaryConst.SECOND_PHOTO_ID, SummaryConst.SECOND_REFERENCE,
                        SummaryConst.SECOND_HEIGHT, SummaryConst.SECOND_WIDTH, SummaryConst.WORSE]
     df_summary = pd.DataFrame(columns=summary_columns)
@@ -108,7 +110,10 @@ def create_summary_excel(products_collection, folder_to_save):
                 second_photo = photos_pair.photo2
                 better_photo = photos_pair.better_photo if photos_pair.better_photo else ""
 
-                df_summary.loc[df_summary.shape[0]] = [getattr(product, "<ID>"), first_photo.name, first_photo.asset_type, first_photo.height, first_photo.width, second_photo.name, second_photo.asset_type, second_photo.height, second_photo.width, better_photo]
+                df_summary.loc[df_summary.shape[0]] = \
+                    [getattr(product, "<ID>"), first_photo.name, first_photo.asset_type,
+                     first_photo.height, first_photo.width, second_photo.name, second_photo.asset_type,
+                     second_photo.height, second_photo.width, better_photo]
 
     # format_excel(df_summary, f"{folder_to_save}/Summary {set_today_date('-')}.xlsx", SUMMARY_FORMAT)
     df_summary.to_excel(f"{folder_to_save}/Summary {set_today_date('-')}.xlsx", index=False)
@@ -119,7 +124,8 @@ def add_column_to_excel(excel_path, products_collection, first_row):
     sheet = workbook.active
 
     last_column = 1
-    while sheet.cell(row=1, column=last_column).value and sheet.cell(row=1, column=last_column).value != "Similarity type":
+    while sheet.cell(row=1, column=last_column).value and sheet.cell(row=1,
+                                                                     column=last_column).value != "Similarity type":
         last_column += 1
 
     if first_row == 0:
@@ -130,7 +136,9 @@ def add_column_to_excel(excel_path, products_collection, first_row):
     i = 0
     for product in products_collection:
         sheet.cell(row=start_row + i, column=last_column).value = product.photos_connection_type
-        sheet.cell(row=start_row + i, column=last_column + 1).value = ";".join(f"{photos_pair.similarity_type},{photos_pair.photo1.name},{photos_pair.photo2.name}" for photos_pair in product.photos_pairs_list)
+        sheet.cell(row=start_row + i, column=last_column + 1).value = ";" \
+            .join(f"{photos_pair.similarity_type},{photos_pair.photo1.name},{photos_pair.photo2.name}"
+                  for photos_pair in product.photos_pairs_list)
         i += 1
     # save the file
     workbook.save(filename=excel_path)
@@ -138,39 +146,55 @@ def add_column_to_excel(excel_path, products_collection, first_row):
 
 def work_with_show_all_summ_excel(products_collection, entry_info):
     folder_to_save = entry_info.photo_path if len(entry_info.photo_path) > 0 else os.path.dirname(entry_info.excel_path)
-    excel_path = f"{folder_to_save}/ShowAllSummary {set_today_date('-')}.xlsx"
+
+    excel_path = f"{folder_to_save}/STEPShowAllSummary {set_today_date('-')}.xlsx" if entry_info.data_from_step \
+        else f"{folder_to_save}/ShowAllSummary {set_today_date('-')}.xlsx"
 
     to_modify = is_summary_excel_exists(excel_path)
 
     if to_modify:
         modify_show_all_summ_excel(products_collection, excel_path)
     else:
-        create_show_all_summ_excel(products_collection, excel_path)
+        create_show_all_summ_excel(products_collection, excel_path, entry_info)
 
 
 def is_summary_excel_exists(excel_path):
     if os.path.isfile(excel_path):
-        if messagebox.askokcancel("Summary excel", "Summary excel exists! Do you want to add new values to existing file?"):
+        if messagebox.askokcancel("Summary excel",
+                                  "Summary excel exists! Do you want to add new values to existing file?"):
             return True
     return False
 
 
-def backup_excel(products_collection, folder_to_save):
+def backup_excel(products_collection, folder_to_save, entry_info):
     excel_path = f"{folder_to_save}/Backup{set_today_date('-')}.xlsx"
-    create_show_all_summ_excel(products_collection, excel_path)
+    create_show_all_summ_excel(products_collection, excel_path, entry_info)
 
 
-def create_show_all_summ_excel(products_collection, excel_path):
-    show_all_summ_columns = [ShowAllConst.PRODUCT_ID, ShowAllConst.PHOTO_ID, ShowAllConst.PHOTO_REFERENCE,
-                             ShowAllConst.HEIGHT, ShowAllConst.WIDTH]
+def get_all_summary_columns(entry_info):
+    if entry_info.data_from_step:
+        return [ShowAllConst.PRODUCT_IDS, ShowAllConst.ASSET_ID, ShowAllConst.PHOTO_REFERENCE_TYPE]
+    else:
+        return [ShowAllConst.PRODUCT_ID, ShowAllConst.PHOTO_ID, ShowAllConst.PHOTO_REFERENCE,
+                ShowAllConst.HEIGHT, ShowAllConst.WIDTH]
+
+
+def create_show_all_summ_excel(products_collection, excel_path, entry_info):
+    show_all_summ_columns = get_all_summary_columns(entry_info)
+
     df_show_all_summ = pd.DataFrame(columns=show_all_summ_columns)
 
     for product in products_collection:
         for photo in product.all_photos:
             if photo.selected_photo:
-                df_show_all_summ.loc[df_show_all_summ.shape[0]] = [product.product_id, photo.name, photo.asset_type, photo.height, photo.width]
+                df_show_all_summ.loc[df_show_all_summ.shape[0]] = product_data_to_summary_excel(product, photo, entry_info)
 
     df_show_all_summ.to_excel(excel_path, index=False)
+
+
+def product_data_to_summary_excel(product, photo, entry_info):
+    return [photo.name, product.product_id, photo.asset_type] if entry_info.data_from_step else \
+        [product.product_id, photo.name, photo.asset_type, photo.height, photo.width]
 
 
 def modify_show_all_summ_excel(products_collection, excel_path):
@@ -185,7 +209,8 @@ def modify_show_all_summ_excel(products_collection, excel_path):
     for product in products_collection:
         for photo in product.all_photos:
             if photo.selected_photo:
-                for column, element in enumerate([product.product_id, photo.name, photo.asset_type, photo.height, photo.width], start=1):
+                for column, element in enumerate([product.product_id, photo.name, photo.asset_type,
+                                                  photo.height, photo.width], start=1):
                     sheet.cell(row=counter, column=column).value = element
 
                 counter += 1
